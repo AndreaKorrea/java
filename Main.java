f4ea47114ff46a76ff9df1aa40e79e060aa4de9b
AndreaKorrea <andreakorrea23@gmail.com>
2025-09-18 19:43:48 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -78,6 +78,10 @@ public class Main {
             moto[i]= new AlquilerMoto(marca, modelo, año, placa);
             // Método estático: NO necesita crear un objeto
             AlquilerMoto.mostrarMotosDisponibles();
+        System.out.println("\n--- Motos registradas---");
+            for(int i=0; i<moto.length; i++){
+                System.out.println("Moto"+ (i+1)+ ":"+moto[i]+ "Alquilada:"+ moto[i].getAlquilada());
+            }
             // Creación de objetos (para acceder a métodos de instancia)
             //AlquilerMoto moto1 = new AlquilerMoto("Yamaha", "MT-07", 2022, "AVG233");
             //AlquilerMoto moto2 = new AlquilerMoto("Honda", "CB500X", 2021, "MAR757");
@@ -95,3 +99,4 @@ public class Main {
         }
     }
 }
+
